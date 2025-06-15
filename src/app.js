c2f6fcebd634756f5292bde3a2a4fca7dffc72c2
Ravish.kumar <ravish2000kumar@gmail.com>
2025-06-15 10:42:17 +0530

    ROUTE handlers

--- src/app.js
+++ src/app.js
@@ -2,16 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use('/test',(req,res)=>{
-    res.send("test is running !");
-});
+// app.use('Route', [RH , RH2], RH3, RH4);
 
-app.use('/hello',(req,res)=>{
-    res.send("hellow from hello path !");
-});
-
-app.use('/',(req,res)=>{
-    res.send("welcome to the dashboard !");
+app.use('/user',
+[(req,res,next)=>
+    {
+    console.log("middleware is running");
+    next();
+}, (req, res,next) => {
+    console.log("this is the first middleware");
+    next();
+}], (req, res,next) => {
+    console.log("this is the second middleware");
+    next();
+}, (req, res,next) => {
+    console.log("this is the third middleware");
+    next();
+}, (req, res) => {
+    console.log("this is the fourth middleware");
+    res.send("this is the 4th middleware response");
 });
 
 
