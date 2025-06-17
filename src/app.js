58602beca90c261d8eb9a476821de6e02c2f080a
Ravish.kumar <ravish2000kumar@gmail.com>
2025-06-17 10:12:56 +0530

    Get the user and feed data

--- src/app.js
+++ src/app.js
@@ -4,13 +4,43 @@ const User = require('./models/user');
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/user',async (req,res) => {
+    const userId = req.body.email
+
+    try {
+        const user = await User.findOne({
+            email : userId
+        })
+        if(user) {
+            res.send(user);
+        } else {
+            res.status(404).send("user not found");
+        }
+    } catch {(err) => {
+        res.status(400).send("something went wrong");
+    }}
+});
+
+// get all user to feed the data to signup user
+app.get('/feed', async (req,res) => {
+    try{
+        const users = await User.find({});
+        if(users) {
+            res.send(users);
+        } else {
+            res.status(404).send("user not found");
+        }
+    } catch {(err) => {
+        res.status(400).send("something went wrong");
+    }}
+})
+
 app.post('/signup', async (req, res) => {
-    const user = new User({
-        firstName : "Ravish",
-        lastName : "Kumar",
-        email: "ravish@kumar",
-        password: "xyz123"
-    });
+
+    // console.log(req.body);
+    const user = new User(req.body);
 
     try {
         await user.save();
