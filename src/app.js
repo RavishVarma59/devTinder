42666d429d45fe0e22c912f1b27188519df6e07d
Ravish.Varma <ravish2000kumar@gmail.com>
2025-08-06 23:33:32 +0530

    added profile request routers

--- src/app.js
+++ src/app.js
@@ -1,47 +1,19 @@
 const express = require('express');
 const connectDb = require('./config/database');
-const User = require('./models/user');
 const cookieParser = require('cookie-parser');
-const { userAuth } = require('./middleware/auth')
 
 const authRouter = require('./routers/auth');
+const profileRouter = require('./routers/profile');
+const requestRouter = require('./routers/request');
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-// Ensure indexes are created
-User.init().then(() => {
-    console.log("Indexes ensured for User schema");
-}).catch((err) => {
-    console.error("Failed to create indexes:", err.message);
-});
 
 app.use('/',authRouter);
-
-
-app.get('/profile', userAuth, async (req, res) => {
-    try {
-
-        const user = req.user;
-
-        res.send(user);
-        
-    } catch (error) {
-        res.status(400).send("error : " + error.message);
-    }
-})
-
-app.post('/sendConnectionRequest', userAuth, async (req, res) => {
-    try {
-        const {user} = req;
-        res.send(user.firstName + " send the connection request");
-    } catch (error) {
-        res.status(400).send("error : " + error.message);
-    }
-
-})
-
+app.use('/',profileRouter);
+app.use('/',requestRouter);
 
 // connecting to database and start listning request
 connectDb().then(()=>{
