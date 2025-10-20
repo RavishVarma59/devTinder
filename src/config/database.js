cda7ef67878a5013b0c2fb852891cc877137b9db
Ravish.Varma <ravish2000kumar@gmail.com>
2025-10-20 17:18:26 +0530

    change mongo db password

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDb = async () => {
-    await mongoose.connect('mongodb+srv://RavishVarma:sY6plonf215aXDfd@namastenodejs.clfy8do.mongodb.net/devTinder')
-
-const indexes = await mongoose.connection.db.collection('users').indexes();
-console.log(indexes);
+    await mongoose.connect('mongodb+srv://RavishVarma:faiqFrOHT3oYEDou@namastenodejs.clfy8do.mongodb.net/devTinder');
 
 }
 
